741578a381a81c71f1affaeb8595310cd48f0df2
dphillips97 <dphillips97@gmail.com>
2018-03-12 13:15:50 -0400

    Added comments

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-#! python 3
 import requests
 from bs4 import BeautifulSoup
 import openpyxl
 import re
 import time
-from naics_codes import *
-
 
 '''
 1st function call. Searches siccode.com for business name (from Excel)
@@ -15,6 +12,7 @@ on the results_page will be the business in Madison Heights/MI.
 # biz_name is from spreadsheet
 def get_site(biz_name):
 	
+	# returns all search results on 1 page
 	search_url = r'https://siccode.com/en/business-list/' + biz_name
 	
 	# get page object, save as string
@@ -34,7 +32,7 @@ def get_site(biz_name):
 2nd function call. Loops thru all <a> links on search results page and 
 uses regex to find link of business in Michigan (starts with 480..).
 '''
-# takes as argument all <a> links from business earch results
+# takes as argument all <a> links from business search results
 def get_url(results_links):
 	
 	for link in results_links:
@@ -58,8 +56,8 @@ def get_url(results_links):
 		
 		# if mo == None:
 		except:
-		
 			pass
+			
 '''
 3rd function call. Calls page specific to business and 
 gets NAICS code on that page. May eventually merge this 
@@ -67,20 +65,27 @@ with 2nd function call.
 '''
 def call_url(biz_url):
 	
+	# calls business-specific page
 	biz_page_url = r'http://siccode.com' + biz_url
 	
+	# converts entire page object to text
 	biz_page = requests.get(biz_page_url)
 	biz_page_doc = biz_page.text
 	
-	
+	# gets all <a> text from page
 	soup_2 = BeautifulSoup(biz_page_doc, 'html.parser')
 	naics_a = soup_2.find_all('a')
 	
+	# for each element in <a> object
 	for elem in naics_a:
 		
+		# change each one into text; move outside for loop?
 		elem_doc = elem.text
+		
+		# gets 6-digit naics code and description after dash
 		elem_regex = r'^\s(\d{6})\s-\s(.*)'
 		
+		# may return None; handle this downstream
 		try:
 		
 			mo = re.search(elem_regex, elem_doc)
@@ -88,12 +93,14 @@ def call_url(biz_url):
 			descr = mo.group(2)
 			
 			return code, descr
+		
 		except:
-			
 			pass
 
 # main loop
 def master():
+	
+	# create workbook & sheet(s) objects
 	wb = openpyxl.load_workbook('biz.xlsx')
 	sheet = wb.active
 	
@@ -121,17 +128,20 @@ def master():
 		try:
 		
 			# uses regex to get naics code from specific website
+			# handle None type below
 			code, descr = call_url(biz_url)
 			
+			# let user know on CLI (mostly for fun)
 			print(' %s - %s' % (str(code), descr))
 			
+			# write results to sheet
 			sheet.cell(row = row_iter, column = max_col + 1, value = code)
 			sheet.cell(row = row_iter, column = max_col + 2, value = descr)
 		
 			# rest for a moment to avoid battering their servers
 			time.sleep(5)
 		
-		# if biz_url is Null
+		# if biz_url is None
 		except:
 			
 			sheet.cell(row = row_iter, column = max_col + 1, value = 'NOT FOUND')
@@ -139,11 +149,11 @@ def master():
 			print(' %s' % 'NOT FOUND')
 		
 		
-		# save temp file every 50 records
-		if (row_iter % 101 == 0):
+		# save temp file every 250 records
+		if (row_iter % 251 == 0):
 			
 			wb.save('biz_temp_%i.xlsx' % (row_iter-1))
-		
+	
 	wb.save('biz_coded.xlsx')
 			
 master()
